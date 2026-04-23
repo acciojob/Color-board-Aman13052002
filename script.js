2bb830be1fcdccb780f1e18956afeadcb3e5cf12
Aman Kumar <125535132+Aman13052002@users.noreply.github.com>
2026-04-23 13:52:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ for(let i=0;i<800;i++){
 	const sq = document.createElement('div');
 	sq.className = "square";
 
-	sq.addEventListener('mouseover',()={
+	sq.addEventListener('mouseover',()=>{
 		const color = `rgb(${Math.random()*256},${Math.random()*256},${Math.random()*256})`;
 		sq.style.backgroundColor = color;
 		sq.style.transition = 'background-color 0s';
 	});
-	sq.addEventListener('mouseout',()={
+	sq.addEventListener('mouseout',()=>{
 		// const color = `rgb(${Math.random()*256},${Math.random()*256},${Math.random()*256})`;
 		sq.style.backgroundColor = 'rgb(29,29,29)';
 		sq.style.transition = 'background-color 1s';
